3b26f21b5433220f201f6b2c518accaafa689835
Gourav Sathe <76203349+GouravSathe@users.noreply.github.com>
2024-05-11 13:53:44 +0000

    updates

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,19 @@
+import dotenv from "dotenv"
+import connectDB from "./db/db.js";
+import {app} from './app.js'
 
-import { app } from './app.js';
+dotenv.config({
+    path: './.env'
+})
 
 
-const PORT = 3000;
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
