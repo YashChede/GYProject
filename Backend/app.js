e836d9d9f46040f085d00cea8d87e5f2655b92b4
Yash Chede <yashchede99@gmail.com>
2024-05-16 21:51:25 +0530

    Adding error,response and handler utils

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,26 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json());
+app.use(express.json({ limit: "16kb" }));
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!, Its Working successfully :)');
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!, Its Working successfully :)");
 });
 
-
-export { app }
+export { app };
